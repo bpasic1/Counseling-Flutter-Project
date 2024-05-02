cdb785373bdb4fec7eab735eeef21a764d513a4a
Benjamin Pašić <bpasic1@etf.unsa.ba>
2024-05-03 00:16:23 +0200

    Starting hamburger menu added.

--- counseling_flutter_app/lib/screens/main_application_screen.dart
+++ counseling_flutter_app/lib/screens/main_application_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +9,11 @@ class MainApplicationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
+    String? userId = user?.uid;
+
     return Scaffold(
+      backgroundColor: Colors.lightBlue.shade50,
       appBar: AppBar(
         title: const Text('Advice Haven'),
         actions: [
@@ -22,6 +27,67 @@ class MainApplicationScreen extends StatelessWidget {
             ),
           )
         ],
+      ),
+      drawer: Drawer(
+        child: FutureBuilder<DocumentSnapshot>(
+          future:
+              FirebaseFirestore.instance.collection('users').doc(userId).get(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (!snapshot.hasData || !snapshot.data!.exists) {
+              return Text('User data not found');
+            } else {
+              var userData = snapshot.data!.data() as Map<String, dynamic>;
+              String username = userData['username'];
+              String email = userData['email'];
+              return ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  UserAccountsDrawerHeader(
+                    accountName: Text(username),
+                    accountEmail: Text(email),
+                    currentAccountPicture: const Icon(
+                      Icons.person,
+                      size: 48,
+                      color: Colors.white,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('Profile'),
+                    onTap: () {
+                      // Navigate to the Profile screen
+                      Navigator.pop(context); // Close the drawer
+                      // You can implement navigation to the Profile screen here
+                    },
+                  ),
+                  ListTile(
+                    title: Text('Reviews'),
+                    onTap: () {
+                      // Navigate to the Reviews screen
+                      Navigator.pop(context); // Close the drawer
+                      // You can implement navigation to the Reviews screen here
+                    },
+                  ),
+                  ListTile(
+                    title: Text('Advices'),
+                    onTap: () {
+                      // Navigate to the Advices screen
+                      Navigator.pop(context); // Close the drawer
+                      // You can implement navigation to the Advices screen here
+                    },
+                  ),
+                  // Add more ListTile widgets for additional options as needed
+                ],
+              );
+            }
+          },
+        ),
       ),
       body: Center(
         child: Column(
